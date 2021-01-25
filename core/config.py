96194134369df380d1170842fc515d08d9f8f623
mojgd <vazifehdoost@kaman.ir>
2021-01-25 22:55:47 +0330

    weighted recall and precision were added

--- core/config.py
+++ core/config.py
@@ -1,6 +1,7 @@
 import os
 import argparse
 
+
 def str2bool(v):
     if v.lower() in ('yes', 'true', 't', 'y', '1'):
         return True
@@ -8,6 +9,7 @@ def str2bool(v):
         return False
     else:
         raise argparse.ArgumentTypeError('Boolean value expected.')
+
 
 def load():
     parser = argparse.ArgumentParser()
@@ -26,14 +28,13 @@ def load():
     # dnn
     parser.add_argument('--num_epochs', default=5, type=int)
 
-
     # all models
     parser.add_argument('--learning_rate', default=0.002, type=float)
 
     # evaluation
-    parser.add_argument('--num_folds', default=10, type=int) # 10
-    #parser.add_argument('--cross_validation', default=False, type=util.str2bool)
-    parser.add_argument('--batch_size', default=256, type=int) #lstm 256 #dnc 1
+    parser.add_argument('--num_folds', default=10, type=int)  # 10
+    # parser.add_argument('--cross_validation', default=False, type=util.str2bool)
+    parser.add_argument('--batch_size', default=256, type=int)  # LSTM 256 #dnc 1
 
     # data
     parser.add_argument('--data_set', default="test.csv")
